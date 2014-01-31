12602bedb139417b0d0a0b2ac060a414731e3c5e
Kim Nguyen <design@kimisgold.com>
2014-01-31 12:55:34 -0500

    Add missing javascript, search views, and mobile stles.

--- common/header.php
+++ common/header.php
@@ -2,6 +2,7 @@
 <html lang="<?php echo get_html_lang(); ?>">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
     <?php if ( $description = option('description')): ?>
     <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
