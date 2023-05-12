730d340181162e5e2c40cbd0c4820f886bb63391
kalbers <ken.albers@gmail.com>
2023-05-12 12:23:17 -0400

    Add translation functionality

--- items/show.php
+++ items/show.php
@@ -20,7 +20,7 @@
     <!-- The following returns all of the files associated with an item. -->
     <?php if ((get_theme_option('Item FileGallery') == 1) && metadata($item, 'has files')): ?>
     <div id="itemfiles" class="element">
-        <h2>Files</h2>
+        <h2><?php echo __('Files'); ?></h2>
         <div class="element-text"><?php echo item_image_gallery(); ?></div>
     </div>
     <?php endif; ?>
@@ -30,7 +30,7 @@
     <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata($item, 'has tags')): ?>
     <div id="item-tags" class="element">
-        <h2>Tags</h2>
+        <h2><?php echo __('Tags'); ?></h2>
         <div class="element-text tags"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif; ?>
@@ -38,14 +38,14 @@
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (get_collection_for_item()): ?>
         <div id="collection" class="element">
-            <h2>Collection</h2>
+            <h2><?php echo __('Collection'); ?></h2>
             <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
         </div>
     <?php endif; ?>
 
     <!-- The following prints a citation for this item. -->
     <div id="item-citation" class="element">
-        <h2>Citation</h2>
+        <h2><?php echo __('Citation'); ?></h2>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
 
